b3a04ddadc935cd11926d4042ca6d937994262b5
Bernhard König <github@diro.ch>
2022-06-22 22:49:06 +0200

    edt

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ console.log('Welcome to the kiwigrid-cloud-ws-api interactive CLI.');
 const kiwigridApi = new KiwigridApi(
   'wss://cloud.kiwigrid.com/kiwibus/1/1/websocket',
   'username',
-  'password'
+  'x'
 );
 kiwigridApi.connect();
